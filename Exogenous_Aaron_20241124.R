1447e560fdd364aa2adb0a30227c0215963d51c0
aabromowitz <aabromowitz@gmail.com>
2024-11-24 15:05:01 -0800

    Added home price index

--- Exogenous_Aaron_20241124.R
+++ Exogenous_Aaron_20241124.R
@@ -463,3 +463,242 @@ f = fore.arima.wge(x,phi=est$phi,theta=est$theta,n.ahead=h.short,lastn=TRUE)
 huc.pred.short = f$f
 f = fore.arima.wge(x,phi=est$phi,theta=est$theta,n.ahead=h.long,lastn=TRUE)
 huc.pred.long = f$f
+
+################################################################################
+
+# House price index
+x = fed_housing_data$Housing_Price_Index 
+plotts.sample.wge(x)
+d = artrans.wge(x,1)
+l = length(x)
+
+# Second difference seems like it could be useful
+d2 = artrans.wge(d,1)
+
+# Maybe even logging as well
+x.log = log(x)
+d.log = artrans.wge(x.log,1)
+d2.log = artrans.wge(d.log,1)
+dev.off()
+
+# Seeing if there is a frequency at 1/4
+parzen.wge(d2.log)
+parzen.wge(d2)
+
+# Differencing out the 0.25 frequency
+factor.wge(c(rep(0,3),1))
+d3 = artrans.wge(d2,c(0,-1))
+d3.log = artrans.wge(d2.log,c(0,-1))
+
+# Wait, did I just do (1-b^4)?
+dev.off()
+parzen.wge(x) # nope, definitely no 0.25 f in there
+
+# What does differencing out the full (1-b^4) do?
+d3.2 = artrans.wge(d2,c(rep(0,3),1))
+
+# First, trying ARMA with d3
+aic5.ar.wge(d3,p=0:160,type='aic',method='burg') # 159 best, 160 highest
+aic5.ar.wge(d3,p=0:160,type='bic',method='burg') # 2 best, 8 highest
+aic5.wge(d3,p=0:10,q=0:2,type='aic') # 5/2 best, p 9 highest, q 2 highest
+aic5.wge(d3,p=0:12,q=0:4,type='aic') # 5/2 best, p 8 highest, q 4 highest
+aic5.wge(d3,p=0:10,q=0:6,type='aic') # 4/5 best, p 6 highest, q 5 highest
+# p    q        aic
+# 4    5   2.318766
+# 3    5   2.318801
+# 5    5   2.331189
+# 6    5   2.335669
+# 2    5   2.343238
+aic5.wge(d3,p=0:6,q=0:5,type='aicc') # 3/5 best, p 6 highest, q 5 highest
+aic5.wge(d3,p=0:6,q=0:5,type='bic') # 0/2 best, p 2 highest, q 5 highest
+# candidates: 4/5, 0/2, 6/5
+
+# d3 ARMA(4,5)
+dev.off()
+est = est.arma.wge(d3,p=4,q=5)
+m = mult.wge(fac1=est$phi,fac2=c(0,-1))
+factor.wge(m$model.coef) # looks good
+roll.win.rmse.wge(x,h.short,d=2,phi=m$model.coef,theta=est$theta) # 6.8122
+roll.win.rmse.wge(x,h.long,d=2,phi=m$model.coef,theta=est$theta) # 41.022
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-f$f)^2)
+ase # 16.01932
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-f$f)^2)
+ase # 2109.469
+
+# d3 ARMA(0,2)
+est = est.arma.wge(d3,p=0,q=2)
+m = mult.wge(fac1=est$phi,fac2=c(0,-1))
+factor.wge(m$model.coef) # looks good
+roll.win.rmse.wge(x,h.short,d=2,phi=m$model.coef,theta=est$theta) # 5.392
+roll.win.rmse.wge(x,h.long,d=2,phi=m$model.coef,theta=est$theta) # 25.866
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-f$f)^2)
+ase # 189.898
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-f$f)^2)
+ase # 9827.962
+
+# d3 ARMA(6,5)
+est = est.arma.wge(d3,p=6,q=5)
+m = mult.wge(fac1=est$phi,fac2=c(0,-1))
+factor.wge(m$model.coef) # looks good
+roll.win.rmse.wge(x,h.short,d=2,phi=m$model.coef,theta=est$theta) # 6.5370
+roll.win.rmse.wge(x,h.long,d=2,phi=m$model.coef,theta=est$theta) # 39.436
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-f$f)^2)
+ase # 14.86637
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-f$f)^2)
+ase # 558.2356
+
+# Try ARMA with d2.log
+aic5.ar.wge(d2.log,p=0:200,type='aic',method='burg') # 195 best, 195 highest
+aic5.ar.wge(d2.log,p=0:195,type='bic',method='burg') # 2 best, 8 highest
+aic5.wge(d2.log,p=0:28,q=0:2,type='aic') # 3/0 best, p 5 highest, q 2 highest
+aic5.wge(d2.log,p=0:8,q=0:4,type='aic') # 3/0 best, p 5 highest, q 2 highest
+# p    q        aic
+# 3    0  -9.124972
+# 4    0  -9.118892
+# 3    1  -9.118074
+# 3    2  -9.109825
+# 5    0  -9.108971
+aic5.wge(d2.log,p=0:5,q=0:2,type='aicc') # 3/0 best, p 4 highest, q 2 highest
+aic5.wge(d2.log,p=0:4,q=0:2,type='bic') # 3/0 best, p 4 highest, q 1 highest
+# candidates: 3/0, 3/2
+
+# d2.log AR(3)
+dev.off()
+est = est.ar.wge(d2.log,p=3)
+roll.win.rmse.wge(x.log,h.short,d=2,phi=est$phi) # 0.016801
+roll.win.rmse.wge(x.log,h.long,d=2,phi=est$phi) # 0.08855
+f = fore.arima.wge(x.log,d=2,phi=est$phi,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-exp(f$f))^2)
+ase # 4.396291
+f = fore.arima.wge(x.log,d=2,phi=est$phi,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-exp(f$f))^2)
+ase # 7244.708
+
+# d2.log ARMA(3,2)
+est = est.arma.wge(d2.log,p=3,q=2)
+roll.win.rmse.wge(x.log,h.short,d=2,phi=est$phi,theta=est$theta) # 0.016905
+roll.win.rmse.wge(x.log,h.long,d=2,phi=est$phi,theta=est$theta) # 0.088613
+f = fore.arima.wge(x.log,d=2,phi=est$phi,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-exp(f$f))^2)
+ase # 19.04625
+f = fore.arima.wge(x.log,d=2,phi=est$phi,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-exp(f$f))^2)
+ase # 7394.011
+
+# Try ARMA with (1-b^4) differenced out
+aic5.ar.wge(d3.2,p=0:200,type='aic',method='burg') # 191 best, 191 highest
+aic5.ar.wge(d3.2,p=0:191,type='bic',method='burg') # 191 best, 191 highest
+aic5.wge(d3.2,p=0:28,q=0:2,type='aic') # 14/1 best, p 17 highest, q 1 highest
+# p    q        aic
+# 14    1   2.470959
+# 16    0   2.473225
+# 16    1   2.481359
+# 15    1   2.481362
+# 17    0   2.481835
+aic5.wge(d3.2,p=0:17,q=0:1,type='aicc') # 14/1 best, p 16 highest, q 1 highest
+aic5.wge(d3.2,p=0:16,q=0:1,type='bic') # 10/1 best, p 12 highest, q 1 highest
+# candidates: 10/1, 14/1
+
+# d3.2 ARMA(10,1)
+est = est.arma.wge(d3.2,p=10,q=1)
+roll.win.rmse.wge(x,h.short,d=2,s=4,phi=est$phi,theta=est$theta) # 5.9488
+roll.win.rmse.wge(x,h.long,d=2,s=4,phi=est$phi,theta=est$theta) # 37.9498
+f = fore.arima.wge(x,d=2,s=4,phi=est$phi,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-f$f)^2)
+ase # 24.94154
+f = fore.arima.wge(x,d=2,s=4,phi=est$phi,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-f$f)^2)
+ase # 9426.235
+
+# d3.2 ARMA(14,1)
+est = est.arma.wge(d3.2,p=14,q=1)
+roll.win.rmse.wge(x,h.short,d=2,s=4,phi=est$phi,theta=est$theta) # 5.9268
+roll.win.rmse.wge(x,h.long,d=2,s=4,phi=est$phi,theta=est$theta) # 37.2596
+f = fore.arima.wge(x,d=2,s=4,phi=est$phi,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-f$f)^2)
+ase # 26.42709
+f = fore.arima.wge(x,d=2,s=4,phi=est$phi,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-f$f)^2)
+ase # 8350.941
+
+# Try signal plus noise with d2
+f = fore.arima.wge(x,d=2,n.ahead=h.short,lastn=TRUE)
+resid = f$resid
+plotts.sample.wge(resid)
+f2 = fore.sigplusnoise.wge(resid,linear=FALSE,freq=0.25,max.p=5,n.ahead=h.short,lastn=TRUE)
+ase = mean((x[(l-h.short+1):l]-f$f-f2$f)^2)
+ase # 1130.361
+f2 = fore.sigplusnoise.wge(resid,linear=FALSE,freq=0.25,max.p=5,n.ahead=h.long,lastn=TRUE)
+ase = mean((x[(l-h.long+1):l]-f$f)^2)
+ase # 24824.99
+
+# MLP
+# d= 0, 5 hidden nodes
+set.seed(10)
+t.train.short= 1:(l-h.short)
+t.test.short=(l-h.short+1):l
+t.train.long= 1:(l-h.long)
+t.test.long=(l-h.long+1):l
+train.short = x[t.train.short]
+test.short = x[t.test.short]
+train.long = x[t.train.long]
+test.long = x[t.test.long]
+fit.mlp = mlp(ts(x))
+plot(fit.mlp)
+roll.win.rmse.nn.wge(x,horizon=h.short,fit.mlp) # 4.24433
+roll.win.rmse.nn.wge(x,horizon=h.long,fit.mlp) # 27.4536
+fit.mlp = mlp(ts(train.short))
+fore.mlp=forecast(fit.mlp,h=h.short)
+plot(seq(1,l,1),x,type="b")
+points(seq(l-h.short+1,l,1),fore.mlp$mean,type="b",pch=15)
+plot(fore.mlp)
+ase=mean((test.short-fore.mlp$mean)^2)
+ase # 499.0734
+fit.mlp = mlp(ts(train.long))
+fore.mlp=forecast(fit.mlp,h=h.long)
+plot(seq(1,l,1),x,type="b")
+points(seq(l-h.long+1,l,1),fore.mlp$mean,type="b",pch=15)
+plot(fore.mlp)
+ase=mean((test.long-fore.mlp$mean)^2)
+ase # 7797.95
+
+# d= 2, 10 hidden nodes
+set.seed(11)
+fit.mlp = mlp(ts(x),difforder=2,hd=10)
+plot(fit.mlp)
+roll.win.rmse.nn.wge(x,horizon=h.short,fit.mlp) # 3.8800
+roll.win.rmse.nn.wge(x,horizon=h.long,fit.mlp) # 24.9757
+fit.mlp = mlp(ts(train.short),difforder=2,hd=10)
+fore.mlp=forecast(fit.mlp,h=h.short)
+plot(seq(1,l,1),x,type="b")
+points(seq(l-h.short+1,l,1),fore.mlp$mean,type="b",pch=15)
+plot(fore.mlp)
+ase=mean((test.short-fore.mlp$mean)^2)
+ase # 1397.616
+fit.mlp = mlp(ts(train.long),difforder=2,hd=10)
+fore.mlp=forecast(fit.mlp,h=h.long)
+plot(seq(1,l,1),x,type="b")
+points(seq(l-h.long+1,l,1),fore.mlp$mean,type="b",pch=15)
+plot(fore.mlp)
+ase=mean((test.long-fore.mlp$mean)^2)
+ase # 7203.322
+
+# Will officially go with ARMA(9,1) predictions
+dev.off()
+x = fed_housing_data$Housing_Price_Index 
+d = artrans.wge(x,1)
+d2 = artrans.wge(d,1)
+d3 = artrans.wge(d2,c(0,-1))
+est = est.arma.wge(d3,p=6,q=5)
+m = mult.wge(fac1=est$phi,fac2=c(0,-1))
+factor.wge(m$model.coef) # looks good
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.short,lastn=TRUE)
+hpi.pred.short = f$f
+f = fore.arima.wge(x,d=2,phi=m$model.coef,theta=est$theta,n.ahead=h.long,lastn=TRUE)
+hpi.pred.long = f$f
